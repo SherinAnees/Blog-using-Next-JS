256b462ab61f2a4b248386c08cc30b534cec9467
unknown <Sherin>
2022-08-28 06:43:57 +0400

    Initial Commit Section Added

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+  return <div className="bg-red-900 font-bold">Footer</div>;
+}
+
+export default Footer;
---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Header />
+      <main>{children}</main>
+
+      <Footer />
+    </>
+  );
+}
