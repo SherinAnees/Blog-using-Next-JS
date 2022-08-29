2b860e1ce0f16538b7ed3cadb0b4f98ceff96ccd
unknown <Sherin>
2022-08-30 00:37:03 +0400

    Display data

--- components/LatestPost.js
+++ components/LatestPost.js
@@ -6,33 +6,33 @@ import fetcher from "../library/fetcher";
 function LatestPost() {
   //fetch data using SWR
   const { data, isLoading, isError } = fetcher(`api/posts`);
-  if (data) console.log(data);
+  if (isLoading) return <div>Loading....</div>;
+  if (isError) return <div>error</div>;
+
   return (
     <section className="container mx-auto md:px-20 py-10">
       <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
 
       {/* grid columns */}
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+        {data?.map((item) => (
+          <Post data={item} key={item.id}></Post>
+        ))}
       </div>
     </section>
   );
 }
 
 export default LatestPost;
-function Post() {
+function Post({ data }) {
+  const { id, title, published, category, author, img } = data;
   return (
     <div className="item">
       <div className="images">
         <Link href={"/"}>
           <a>
             <Image
-              src={"/images/img1.jpg"}
+              src={img || "/"}
               className="rounded"
               width={500}
               height={350}
@@ -44,17 +44,19 @@ function Post() {
         <div className="cat">
           <Link href={"/"}>
             <a className="text-orange-600 hover:text-orange-800">
-              Business, Travel
+              {category || "Unknown"}
             </a>
           </Link>
           <Link href={"/"}>
-            <a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a>
+            <a className="text-gray-800 hover:text-gray-600">
+              {published || "Unknown"}
+            </a>
           </Link>
         </div>
         <div className="title">
           <Link href={"/"}>
             <a className="text-xl font-bold text-gray-800 hover:text-gray-600">
-              Your most unhappy customers are your greatest source of learning
+              {title || "Unknown"}
             </a>
           </Link>
         </div>
@@ -64,7 +66,7 @@ function Post() {
           text by the name of Lorem Ipsum decided to leave for the far World of
           Grammar.
         </p>
-        <Author />
+        {author ? <Author /> : <></>}
       </div>
     </div>
   );
